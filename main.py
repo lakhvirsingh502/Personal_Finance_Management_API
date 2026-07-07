588eeaeabbd84259cf2a1eef1cf456bb76516f6e
lakhvirsingh502 <lakhvirheer098@gmail.com>
2026-07-07 15:36:38 -0400

    homepage

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ app = FastAPI(
     version="1.0.0"
 
 )
+@app.get("/")
+def home():
+    return {"message": "Personal Finance Management API is running"}
 app.include_router(auth.router)
 app.include_router(admin.router)
 app.include_router(user.router)
